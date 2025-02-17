e4e91f2a0fb2f6de5595f5fbb3c47decc33335f0
jisangaaa <wltkd6881@naver.com>
2025-02-17 16:48:56 +0900

    0217

---
+++ 3 week/game/graphic/__init__.py
@@ -0,0 +1,6 @@
+VERSION = 3.7
+
+def print_version_info():
+    print(f'The version graphic of this game is {VERSION}')
+
+__all__ = ['echo']
---
+++ 3 week/game/sound/__init__.py
@@ -0,0 +1,4 @@
+VERSION = 3.5
+
+def print_version_info():
+    print(f'The version sound of this game is {VERSION}')
