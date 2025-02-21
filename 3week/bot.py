127ebfdc420c7fb45b32ad5e82d162ffbd8f7219
jisangaaa <wltkd6881@naver.com>
2025-02-21 17:16:50 +0900

    0221

--- 3week/bot.py
+++ 3week/bot.py
@@ -1,13 +1,15 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
+import tlak_db as tk
 
-TOKEN = "7625750945:AAFTUOibK4_AhYQc-z5eCZnfmpNJEKY_jeodwqst"
 
-TRIGGER_WORDS = {
-    "안녕":"안녕하세요! 저는 지상이 입니다.",
-    "루다":"안녕하세요! 저는 루다 입니다.",
-    "라면":"안녕하세요! 저는 너구리 입니다."
-}
+TOKEN = "7625750945:AAFTUOibK4_AhYQc-z5eCZnfmpNJEKY_jeofdgdfg"
+
+#TRIGGER_WORDS = {
+#    "안녕":"안녕하세요! 저는 지상이 입니다.",
+#    "루다":"안녕하세요! 저는 루다 입니다.",
+#    "라면":"안녕하세요! 저는 너구리 입니다."
+#}
 
 async def start(update,context):
     await update.message.reply_text("안녕하세요 저는 루다입니다. 무엇을 도와드릴까요?")
@@ -15,7 +17,7 @@ async def monitor_chat(update,context):
     user_text = update.message.text #감지된 메세지들
     chat_id = update.message.chat_id #메세지가 온 채팅방
 
-    for key, res in TRIGGER_WORDS.items():
+    for key, res in tk.TRIGGER_WORDS.items():
         if key in user_text:
             await context.bot.send_message(chat_id=chat_id, text=res)
             break    #한개의 키워드에만 반응
